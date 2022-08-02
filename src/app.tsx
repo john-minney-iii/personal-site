3ad076d6628bae75fa285c731b80a264a8ccfef7
john-minney-iii <jminneyiii@gmail.com>
2022-08-01 18:39:14 -0600

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = () => {
+    // =========================================================
+    return <div>
+        <h1>John Minney III</h1>
+    </div>;
+};
+
+export default App;
