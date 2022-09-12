9dc78062af8ce48174abd4ca9e21195d5e1d25c7
john-minney-iii <jminneyiii@gmail.com>
2022-09-12 14:10:29 -0600

    Testing

--- src/views/landingView.tsx
+++ src/views/landingView.tsx
@@ -4,6 +4,7 @@ const LandingView : React.FunctionComponent = () => {
     // =====================================================
     return <div className="h-100">
         <h1>Landing Page</h1>
+	<p>Placeholder text</p>
     </div>;
 };
 
