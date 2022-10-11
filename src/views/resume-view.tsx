c6f8db349d23ce6290c1ee9fdb13c7121bcce5ec
john-minney-iii <jminneyiii@gmail.com>
2022-10-10 21:59:15 -0600

    Done for the night

--- src/views/resume-view.tsx
+++ src/views/resume-view.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import ProfilePicture from "../assets/images/profile-picture.jpg";
 
 const ResumeView = () => {
     // =====================================================
-    return <div className="h-100 d-flex justify-content-center align-items-center flex-column">
-        <h1>Resume View</h1>
-        <p>
-            Under construction, please check back later.
-        </p>
+    return <div className="h-100 d-flex align-items-center flex-column mt-5">
+        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="w-25 d-flex justify-content-center">
+                    <img className="w-50 rounded-circle" src={ProfilePicture} alt="hey look its me" />
+                </div>
+            </div>
+        </div>
+        <div className="row d-flex justify-content-center flex-column">
+            <p className="h1 fw-normal">Resume</p>
+        </div>
     </div>;
 };
 
